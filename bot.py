3386f5d14277c58f1db72d1265f5ae5903ff61cd
João Guimarães <joao.paulo.f.guimaraes@gmail.com>
2022-01-13 14:42:39 -0300

    gerando arquivo com nome diferentes

--- bot.py
+++ bot.py
@@ -1,15 +1,37 @@
 import subprocess
 
-nome_aluno = "fulano"
-valor_aluno = str(47)
+
+def criar_arquivo_auxiliar(nome,valor):
+    f = open("automacao.tex", "w")
+    nome = "\\newcommand{\\nomealuno}{" + nome_aluno + "}\n"
+    valor = "\\newcommand{\\valorquestao}{" + valor_aluno + "}"
+    f.write(nome)
+    f.write(valor)
+    f.close()
 
 
-f = open("automacao.tex", "w")
-nome = "\\newcommand{\\nomealuno}{" + nome_aluno + "}\n"
-valor = "\\newcommand{\\valorquestao}{" + valor_aluno + "}"
-f.write(nome)
-f.write(valor)
-f.close()
+def gerar_pdf(nome_arquivo):
+    aux = nome_arquivo = ".pdf"
+    subprocess.run(["pdflatex", "main.tex"])
+    subprocess.run(["mv", "main.pdf", aux])
 
 
-subprocess.run(["pdflatex", "main.tex"])
+lista_de_alunos = ["fulano", "cicrano", "beltrano"]
+lista_de_valores = ["13", "47", "22"] 
+
+
+cont = 0
+N = len(lista_de_alunos)
+
+print(N)
+while (cont < N):
+    nome_aluno = lista_de_alunos[cont]
+    valor_aluno = str(lista_de_valores[cont])    
+    criar_arquivo_auxiliar(nome_aluno,valor_aluno)  
+    gerar_pdf(nome_aluno)
+    cont = cont +1
+
+
+
+
+
